00635351fbf6951007311a4af572cca94e265e3d
Pedro Batista <pedro@yellowberry.com.br>
2023-01-02 11:14:18 -0300

    key problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,8 @@ export function App() {
         <main>
           {posts.map(post => {
             return (
-              <Post 
+              <Post
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
