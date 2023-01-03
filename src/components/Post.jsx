ac84c212eefc2bb238a18217f35e5768e35d7723
Pedro Batista <pedro@yellowberry.com.br>
2023-01-03 09:54:34 -0300

    working on delete comment button

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentario: ${comment}`);
+  }
+
   return(
     <article className={ styles.post }>
       <header>
@@ -82,7 +86,13 @@ export function Post({ author, publishedAt, content }) {
     
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment}/>
+          return (
+            <Comment 
+              key={comment} 
+              content={comment} 
+              onDeleteComment={deleteComment} 
+            />
+          )
         })}
       </div>
     </article>
